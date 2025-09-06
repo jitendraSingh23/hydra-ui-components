84803f10649a8b889507408c862eac71e2a84a09
jitendra <jitendrasingh2ws@gmail.com>
2025-09-06 13:52:31 +0530

    button fixed

--- components/Button/types.ts
+++
@@ -1,9 +0,0 @@
-export interface ButtonProps {
-  children: React.ReactNode;
-  variant?: "primary" | "secondary" | "outline" | "ghost";
-  size?: "small" | "medium" | "large";
-  disabled?: boolean;
-  onClick?: () => void;
-  className?: string;
-  [key: string]: any;
-}
